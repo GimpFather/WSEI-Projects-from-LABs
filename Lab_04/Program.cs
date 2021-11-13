d0cb3f009d07fa83853ddeec3708d80debe8ba6c
filip.galczyk <filip.galczyk@PK4-04>
2021-11-13 12:12:19 +0100

    Dodaj pliki projektów.

---
+++ Lab_04/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tutaj będziemy robić rzeczy na studia!");
+        }
+    }
+}
